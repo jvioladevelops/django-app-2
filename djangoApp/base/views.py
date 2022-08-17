469cd1c10293d8de4e3353d7c30dd18217359ef7
Justin Viola <justinsviola@gmail.com>
2022-08-16 20:58:12 -0400

    separate html into 'base', create views and models, setup initial post submit functionality

--- djangoApp/base/views.py
+++ djangoApp/base/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
+
+# rooms = [
+#     {'id' :1, 'name': 'Lets learn python!'},
+#     {'id' :2, 'name': 'Lets learn something!'},
+#     {'id' :3, 'name': 'Lets learn new!'},
+# ]
 
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        # The form already knows which values to extract
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # since the name is in url.py I can access the home url by just the name
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
