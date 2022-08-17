b6f5e20fc9adfcdcf711b2aef31f7a0436b48c6c
Justin Viola <justinsviola@gmail.com>
2022-08-16 21:54:03 -0400

    delete and update functionality

--- djangoApp/base/views.py
+++ djangoApp/base/views.py
@@ -30,3 +30,26 @@ def createRoom(request):
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    #making sure the primary key patcahes to prefill form
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        #the data from POST will replace whatever data for room
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
