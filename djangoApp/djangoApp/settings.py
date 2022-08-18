c187cdf2442acee0d54e50d36a386ed8812bd0bc
Justin Viola <justinsviola@gmail.com>
2022-08-18 00:20:03 -0400

    installing styled templates

--- djangoApp/djangoApp/settings.py
+++ djangoApp/djangoApp/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'base.apps.BaseConfig',
+    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
